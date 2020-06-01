b34e71be13779bbe48693d7527b6f8366d200532
elsa chen <snowing222@gmail.com>
2020-06-01 21:57:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -4,7 +4,7 @@ class Transfer
     @sender=sender
     @receiver=receiver
     @status="pending"
-    @amount=transer_amount
+    @amount=amount
   end
 
   def valid?
@@ -12,9 +12,25 @@ class Transfer
   end
 
   def execute_transaction
+    if self.valid? && self.sender.balance>=self.amount&&self.status!="complete"
+      self.sender.balance=self.sender.balance-amount
+      self.receiver.balance=self.receiver.balance+amount
+      self.status="complete"
+    else
+      self.status="rejected"
+      "Transaction rejected. Please check your account balance."
+
+    end
+
+
   end
 
   def reverse_transfer
+    if self.status="complete"
+      self.sender.balance=self.sender.balance+amount
+      self.receiver.balance=self.receiver.balance-amount
+      self.status="reversed"
+    end
   end
 
 
